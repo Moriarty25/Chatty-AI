58de025d206eb7a2dec18aa3159dd19eb0f584fb
Moriarty25 <afanasev.andrey99@gmail.com>
2023-07-21 12:13:13 +0300

    feat: added scss added example component

---
+++ src/components/TestScss/TestScss.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from './TestScss.module.scss'
+
+type TTestScssProps = {
+	text: string;
+}
+
+export const TestScss:FC<TTestScssProps> = ({text}) => {
+	return (
+		<p className={styles.paragraph}>
+			{text}
+		</p>
+	)
+}
