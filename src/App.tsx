5b184af151dc85774ce4fd7b960096a02a50cc25
Moriarty25 <afanasev.andrey99@gmail.com>
2023-07-25 14:20:02 +0300

    feat: added useLocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TestScss } from './components/TestScss/TestScss'
+import useLocalStorage from 'use-local-storage'
 
 function App() {
 	const [count, setCount] = useState(0)
+	const [language, setLanguage] = useLocalStorage('language', '')
 
 	return (
 		<>
@@ -19,13 +21,13 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button onClick={() => setLanguage('Русский')}>
           count is {count}
 				</button>
 				<p>
           Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
-				<TestScss text='Demo variables scss!'/>
+				<TestScss text={language}/>
 			</div>
 			<p className="read-the-docs">
         Click on the Vite and React logos to learn more
